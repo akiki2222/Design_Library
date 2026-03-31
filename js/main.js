09160b3c21738afea78651761973fb4754c6b972
akiki2222 <akiki.2222@gmail.com>
2026-03-31 12:38:27 +0900

    ポートフォリオ６？

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-const filters = document.querySelectorAll(".filter button");
-const items = document.querySelectorAll(".works-item");
+$(document).ready(function () {
+  var modal = $('[data-remodal-id="works-modal"]').remodal();
 
-filters.forEach((filter) => {
-  filter.addEventListener("click", () => {
-    const selected = filter.textContent.toLowerCase();
+  $(".filter button").on("click", function () {
+    var selected = $(this).text().toLowerCase();
 
-    items.forEach((item) => {
-      if (selected === "all" || item.dataset.category === selected) {
-        item.classList.remove("inactive");
+    $(".works-item").each(function () {
+      if (selected === "all" || $(this).data("category") === selected) {
+        $(this).removeClass("inactive");
       } else {
-        item.classList.add("inactive");
+        $(this).addClass("inactive");
       }
     });
   });
-});
 
-$(document).ready(function(){
-  var modal = $('[data-remodal-id="works-modal"]').remodal();
-
-  $('.works-item').on('click', function(){
-    $('#remodal-img').attr('src', $(this).data('img'));
+  $(".works-item").on("click", function () {
+    $("#remodal-img").attr("src", $(this).data("img"));
+    $("#remodal-title").text($(this).data("title"));
+    $("#remodal-client").text($(this).data("client"));
+    $("#remodal-tool").text($(this).data("tool"));
+    $("#remodal-size").text($(this).data("size"));
+    $("#remodal-purpose").text($(this).data("purpose"));
+    $("#remodal-concept").text($(this).data("concept"));
+    $("#remodal-points").text($(this).data("points"));
+    $("#remodal-target").text($(this).data("target"));
+    $("#remodal-media").text($(this).data("media"));
     modal.open();
   });
+});
+$('form').on('submit', function(e){
+  e.preventDefault();  // ← 通常の送信をいったん止める
+  var form = this;
+  setTimeout(function(){
+    form.submit();  // ← フォームを送信
+    window.location.href = 'thanks.html';  // ← ページ遷移
+  }, 100);
 });
