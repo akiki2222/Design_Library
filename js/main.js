94f0c103f6c97b7405d603f47007eccb540fe122
akiki2222 <akiki.2222@gmail.com>
2026-04-07 15:35:54 +0900

    ポートフォリオ８

--- js/main.js
+++ js/main.js
@@ -18,19 +18,39 @@ filters.forEach((filter) => {
 $(document).ready(function () {
   var modal = $('[data-remodal-id="works-modal"]').remodal();
 
-  $(".works-item").on("click", function () {
-    $("#remodal-img").attr("src", $(this).data("img"));
-    $("#remodal-title").text($(this).data("title"));
-    $("#remodal-client").text($(this).data("client"));
-    $("#remodal-tool").text($(this).data(""));
-    $("#remodal-size").text($(this).data("size"));
-    $("#remodal-purpose").text($(this).data("purpose"));
-    $("#remodal-concept").text($(this).data("concept"));
-    $("#remodal-points").text($(this).data("points"));
-    $("#remodal-target").text($(this).data("target"));
-    $("#remodal-media").text($(this).data("media"));
-    modal.open();
-  });
+  var currentIndex = 0;
+var items = [];
+
+$(".works-item").on("click", function () {
+  items = $(".works-item").toArray();
+  currentIndex = items.indexOf(this);
+  showModal(currentIndex);
+  modal.open();
+});
+
+function showModal(index) {
+  var item = $(items[index]);
+  $("#remodal-img").attr("src", item.data("img"));
+  $("#remodal-title").text(item.data("title"));
+  $("#remodal-client").text(item.data("client"));
+  $("#remodal-tool").text(item.data("tool"));
+  $("#remodal-size").text(item.data("size"));
+  $("#remodal-purpose").text(item.data("purpose"));
+  $("#remodal-concept").text(item.data("concept"));
+  $("#remodal-points").text(item.data("points"));
+  $("#remodal-target").text(item.data("target"));
+  $("#remodal-media").text(item.data("media"));
+}
+
+$(".remodal-prev").on("click", function () {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  showModal(currentIndex);
+});
+
+$(".remodal-next").on("click", function () {
+  currentIndex = (currentIndex + 1) % items.length;
+  showModal(currentIndex);
+});
   // ハンバーガーメニュー
   const hamburger = document.getElementById("hamburger");
   const spNav = document.getElementById("sp_nav");
